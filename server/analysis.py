4ec5f2855cdcc8283a736f448c22248c445917e7
rexos <alex.pellegrini@live.com>
2013-11-18 17:57:01 +0100

    updated analysis.py

--- server/analysis.py
+++ server/analysis.py
@@ -23,28 +23,27 @@ class Analyzer():
 
     def __init__(self):
         import zipfile
-        self.H = {}
-        self.S = {}
+        self.list = {}
         self.parser = Parser()
+        self.afinn_url = "http://www2.imm.dtu.dk/pubdb/views/edoc_download.php/6010/zip/imm6010.zip"
         self.url = "https://dl.dropbox.com/u/3773091/Twitter%20Sentiment/Twitter%20sentiment%20analysis.zip"
-        urllib.urlretrieve( self.url, 'word_list.zip' )
+        urllib.urlretrieve( self.afinn_url, 'word_list.zip' )
         zip = zipfile.ZipFile('word_list.zip')
-        list = zip.extract('twitter_sentiment_list.csv')
-        with open(list, 'r') as file:
-            file.readline()
-            for line in file:
-                data = line[:-1].split(',')
-                self.H[data[0]] = float(data[1])
-                self.S[data[0]] = float(data[2])
+        self.list = dict(map(lambda (k,v): ( unicode(k, 'utf-8'), int(v) ), 
+                 [ line.split('\t') for line in open(zip.extract('AFINN/AFINN-111.txt')) ]))
+        
         # clean temporary files on the fly
         os.remove('word_list.zip')
-        os.remove('twitter_sentiment_list.csv')
+        os.remove('AFINN/AFINN-111.txt')
+        os.rmdir('AFINN')
 
     def analyze(self, tweet):
     #evaluates the log probability of tweet happiness ( sadness = 1 - happiness )
         tweet = self.parser.parse(tweet)
-        tw_happy = [self.H.get(word,0) for word in tweet]
-        tw_sad = [self.S.get(word,0) for word in tweet]
-        happy_prob = np.reciprocal( np.exp( np.sum(tw_sad) - np.sum(tw_happy) ) + 1 )
-        return happy_prob
+        tw_values = [self.list.get(word,0) for word in tweet]
+        values = [ int(x) for x in tw_values if x != 0 ]
+        length = ( len(values) if len(values) else 1 )
+        mean = ( sum( tw_values ) ) / float( length )
+        weigth = ( mean + 5 ) / 10.
+        return weigth
 
